ce59a1a3f9eb1297168fd538210338bf2787d4c3
Aaron Garate <agarate@outlook.cl>
2026-07-20 13:15:22 -0400

    feat: initialize service worker with Workbox precaching and navigation routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p8jn00114gc"
+    "revision": "0.4qd7jsqsvhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
